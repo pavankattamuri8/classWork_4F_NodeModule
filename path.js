b3ecefeadb578c7cc5da5c1f5f2819e8e518bbe7
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2024-09-23 15:31:07 +0530

    Update path.js

--- path.js
+++ path.js
@@ -1,5 +1,5 @@
 const path=require('path')
-const pathFile="D:\\Learn_MERN\\Node\\path.js";
+const pathFile="C:\\Users\\User\\Desktop\\x\\NODE\\path.js";
 console.log(path.dirname(pathFile))
 console.log(path.basename(pathFile))
 console.log(path.extname(pathFile))
